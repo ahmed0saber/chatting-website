d83e2e02691517a20e6b58d9136819f8a2029166
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-12 18:38:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -32,3 +32,24 @@ function check_screen(){
         document.getElementsByClassName("chats")[0].style.display = "none";
     }
 }
+
+var toggled = true;
+function toggle(){
+    if(toggled){
+        document.getElementsByClassName("drop-down")[0].style.display = "block";
+        document.getElementsByClassName("fa-angle-down")[0].classList.add("fa-angle-up");
+        document.getElementsByClassName("fa-angle-down")[0].classList.remove("fa-angle-down");
+    }else{
+        document.getElementsByClassName("drop-down")[0].style.display = "none";
+        document.getElementsByClassName("fa-angle-up")[0].classList.add("fa-angle-down");
+        document.getElementsByClassName("fa-angle-up")[0].classList.remove("fa-angle-up");
+    }
+    toggled = !toggled;
+}
+
+function open_side(){
+    document.getElementsByClassName("side-menu")[0].style.display = "flex";
+}
+function close_side(){
+    document.getElementsByClassName("side-menu")[0].style.display = "none";
+}
